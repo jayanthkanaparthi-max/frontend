acc492384152b275d4e9a2f3e2482b0e1bb45acf
jayanth <jayanthkanaparthi@gmail.com>
2025-10-20 14:58:45 +0530

    first

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'srv-d3qvs8vdiees73am9240/api';
 
 // Create axios instance
 const api = axios.create({
